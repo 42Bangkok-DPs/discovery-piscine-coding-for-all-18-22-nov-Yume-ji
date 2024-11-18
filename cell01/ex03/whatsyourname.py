1cfbb52df75898ad85d8fe40f59aa1fe3daa4f2b
Yume-ji <twilightyume@gmail.com>
2024-11-18 15:40:46 +0000

    main

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,4 +1,4 @@
 x = input("Hey, what's your first name? : ")
 y = input("And your last name? : ")
 
-print("Well, pleased to meet you, " + x + " " + y)
+print("Well, pleased to meet you, " + x + " " + y + ".")
