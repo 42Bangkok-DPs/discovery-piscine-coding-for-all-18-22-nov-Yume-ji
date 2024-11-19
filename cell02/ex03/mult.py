cb4988c038e3587869fcef53bd9e63c92592ceee
Yume-ji <twilightyume@gmail.com>
2024-11-19 12:00:03 +0000

    main

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+print("Enter the first number:")
+first_number = float(input())
+
+print("Enter the second number: ")
+second_number = float(input())
+
+third_number = first_number * second_number
+print(str(first_number) + " x " + str(second_number) + " = " + str(third_number))
+
