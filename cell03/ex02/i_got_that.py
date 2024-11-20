abc29dc1f3f619c8b1390a26adcf6f1ce915109d
Yume-ji <twilightyume@gmail.com>
2024-11-20 12:51:07 +0000

    main

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+ans = str(input("What you gotta say? : "))
+
+while ans != "STOP":
+    ans = str(input("I got that! Anything else? : "))
+    if ans == "STOP":
+        break
