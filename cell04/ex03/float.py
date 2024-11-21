341b6544c29e4336e97f792ce2e3d2efb2390401
Yume-ji <twilightyume@gmail.com>
2024-11-21 03:09:21 +0000

    main

--- cell04/ex03/float.py
+++ cell04/ex03/float.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 n = input("Give me a number: ")
 
+try:
+    number = float(n)
+    if number.is_integer():
+        print("This number is an integer.")
+    else:
+        print("This number is a decimal.")
+
+except ValueError:
+    print("This isn't a number.")
